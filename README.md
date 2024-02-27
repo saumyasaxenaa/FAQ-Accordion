9b9f6198758d9149fc3e579e2fb45274e931f1d2
saumyasaxenaa <saumyasaxena004@gmail.com>
2024-02-26 20:19:45 -0800

    setup react tabs

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# FAQ/Accordion
+# React Projects
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
-import Question from "./Question";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import Accordian from "./accordion/Accordion";
 
 const App = () => {
   return (
-    <>
-      <h1>FAQ/Accordian</h1>
-      <div className="container">
-        <h2>Frequently Asked Questions</h2>
-        <div className="questions">
-          {questions.map((question) => (
-            <Question key={question.id} question={question} />
-          ))}
-        </div>
-      </div>
-    </>
+    <Tabs>
+      <TabList>
+        <Tab>Title 1</Tab>
+        <Tab>Title 2</Tab>
+      </TabList>
+      <TabPanel>{<Accordian />}</TabPanel>
+      <TabPanel>
+        <h2>Content</h2>
+      </TabPanel>
+    </Tabs>
   );
 };
 export default App;
-
-const questions = [
-  {
-    id: 1,
-    title: "Is this a good product?",
-    info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem.",
-  },
-  {
-    id: 2,
-    title: "How much does it cost?",
-    info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem.",
-  },
-  {
-    id: 3,
-    title: "When can I get it?",
-    info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem.",
-  },
-];
